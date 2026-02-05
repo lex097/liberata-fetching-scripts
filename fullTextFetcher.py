cfee5cec83b9a2ecf620943f4e76515a1cafb1af
Lex Gu <lyg6@duke.edu>
2026-02-05 14:42:42 -0500

    basic stuff

---
+++ fullTextFetcher.py
@@ -0,0 +1,66 @@
+import os
+import json
+from tokenize import String
+from dotenv import load_dotenv
+from supabase import create_client, Client
+from typing import Dict, List, Optional, Tuple, Any, Union
+
+
+def initialize_supabase_client(
+    supabase_url: Optional[str] = None,
+    supabase_key: Optional[str] = None,
+    force_reinit: Optional[bool] = False,
+    use_dotenv: Optional[bool] = True,
+) -> Client:
+    """
+    Initialize and return a Supabase client.
+
+    Parameters
+    ----------
+    supabase_url, supabase_key : Optional[str]
+        Overrides for env vars.
+    force_reinit : bool
+        If True, recreate the client even if one exists.
+    use_dotenv : bool
+        If True, call load_dotenv() to populate environment variables.
+
+    Returns
+    -------
+    supabase.Client
+    """
+
+    # return client if one exists already
+    global supabase
+    if not force_reinit and 'supabase' in globals() and supabase is not None:
+        return supabase
+
+    if use_dotenv:
+        load_dotenv()
+
+    url = supabase_url or os.getenv('SUPABASE_URL')
+    key = supabase_key or os.getenv('SUPABASE_KEY')
+    if not url or not key:
+        raise RuntimeError("Supabase credentials not found. Set SUPABASE_URL and SUPABASE_KEY or pass overrides")
+
+    try:
+        supabase = create_client(url, key)
+        return supabase
+    except Exception:
+        supabase = None
+        raise
+
+def getFullText(db: Client, id):
+    response = db.table('manuscripts').select('*').eq('id', id).single().execute()
+    return response
+
+
+
+def main():
+    db = initialize_supabase_client()
+    print(getFullText(db, '000776d4-5f16-4213-a54e-8329ceb88872'))
+
+
+
+
+if __name__ == '__main__':
+    main()
